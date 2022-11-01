169b8abb5fcc2dd531c89810e96c7901e1e3064a
Paulina Gonzalez <paulinagzzc@gmail.com>
2022-11-01 16:05:13 -0400

    Add HTML structure and JS dynamic list

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,14 +1,25 @@
 import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+//   // Lodash, now imported by this script
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+const listContainer = document.querySelector('.list-container');
+const tasks = [
+  { index: 0, description: 'Wash the dishes', completed: true },
+  { index: 1, description: 'Buy groceries', completed: true },
+  { index: 2, description: 'Do the laundry', completed: false },
+];
 
-  return element;
-}
+const createTask = (tasks) => {
+  const taskContent = `<div class="task">Hello ${tasks.description}</div>`;
 
-document.body.appendChild(component());
+  listContainer.innerHTML += taskContent;
+};
+
+const createList = (arr) => {
+  for (let i = 0; i < arr.length; i += 1) {
+    createTask(arr[i]);
+  }
+};
+
+createList(tasks);
