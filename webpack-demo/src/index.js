831ee5555915f779f0c36ef5fd93d501adf0481d
Paulina Gonzalez <paulinagzzc@gmail.com>
2022-11-02 00:29:08 -0400

    Complete styling and CSS of to do list

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,9 +1,24 @@
 import _ from 'lodash';
 import './style.css';
+import Dots from './images/dots.png';
+import Enter from './images/enter.png';
+import Refresh from './images/refresh.png';
 
 //   // Lodash, now imported by this script
+const refreshContainer = document.querySelector('.refresh-container');
+const refresh = new Image();
+refresh.src = Refresh;
+refreshContainer.innerHTML = `<img src=${Refresh} class="refresh-icon" />`;
+
+const enterContainer = document.querySelector('.enter-container');
+const enter = new Image();
+enter.src = Enter;
+enterContainer.innerHTML = `<img src=${Enter} class="enter-icon" />`;
 
 const listContainer = document.querySelector('.list-container');
+const dots = new Image();
+dots.src = Dots;
+
 const tasks = [
   { index: 0, description: 'Wash the dishes', completed: true },
   { index: 1, description: 'Buy groceries', completed: true },
@@ -11,7 +26,12 @@ const tasks = [
 ];
 
 const createTask = (tasks) => {
-  const taskContent = `<div class="task">Hello ${tasks.description}</div>`;
+  const taskContent = `
+  <li class="element">
+      <input type="checkbox" id="checkbox" class="checkbox" />
+      <div>${tasks.description}</div>
+      <div class="dots-container"><img class="dots"src=${Dots} /></div>
+  </li>`;
 
   listContainer.innerHTML += taskContent;
 };
